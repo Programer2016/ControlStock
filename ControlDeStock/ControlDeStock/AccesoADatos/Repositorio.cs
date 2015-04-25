f59520885a15de27e46601510940e8f96f6c0938
JaviMarciano <jmarchetti@convey.com>
2015-04-25 15:18:35 -0300

    Se Agregó el repositorio generico

---
+++ ControlDeStock/ControlDeStock/AccesoADatos/Repositorio.cs
@@ -0,0 +1,72 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoADatos
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        ContextoDb dbContexto;
+
+        public Repositorio(ContextoDb dbContexto)
+        {
+            this.dbContexto = dbContexto;
+        }
+
+        public Repositorio()
+        {
+            dbContexto = new ContextoDb();
+        }
+
+        public IQueryable<T> Listar()
+        {
+            return dbContexto.Set<T>();
+        }
+
+        public IQueryable<T> ObtenerPor(System.Linq.Expressions.Expression<Func<T, bool>> predicado)
+        {
+
+            return dbContexto.Set<T>().Where(predicado);
+
+        }
+
+        public T ObtenerPorId(int Id)
+        {
+            return dbContexto.Set<T>().Find(Id);
+        }
+
+        public void Agregar(T entidad)
+        {
+            dbContexto.Set<T>().Add(entidad);
+            dbContexto.SaveChanges();
+        }
+
+        public T Modificar(T entidad, int key)
+        {
+            if (entidad == null)
+                return null;
+
+            T existing = dbContexto.Set<T>().Find(key);
+            if (existing != null)
+            {
+                dbContexto.Entry(existing).CurrentValues.SetValues(entidad);
+                dbContexto.SaveChanges();
+            }
+            return existing;
+        }
+
+        public void Eliminar(T entidad)
+        {
+            dbContexto.Set<T>().Remove(entidad);
+            dbContexto.SaveChanges();
+        }
+
+        public void Guardar()
+        {
+            dbContexto.SaveChanges();
+        }
+    }
+}
---
+++ ControlDeStock/ControlDeStock/Entidades/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public interface IRepositorio<T>
+    {
+        IQueryable<T> Listar();
+        IQueryable<T> ObtenerPor(System.Linq.Expressions.Expression<Func<T, bool>> predicado);
+        void Guardar();
+        T ObtenerPorId(int Id);
+        void Agregar(T entidad);
+        T Modificar(T entidad, int key);
+        void Eliminar(T entidad);
+    }
+}
